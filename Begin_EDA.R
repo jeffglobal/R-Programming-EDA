299f9ba5679a1531e60437aa5ce61256ef567843
Jeffrey Spinner <jeffglobalmedic@gmail.com>
2018-06-26 08:07:47 -0400

    Added Normality Testing

--- Begin_EDA.R
+++ Begin_EDA.R
@@ -1,31 +1,55 @@
 # Beginning Data Analysis
 
-library(ggplot2)
+require(ggplot2)
+require(mgcv)
 
 # Data set
 
 str(Orange)
 summary(Orange)
 
+
+
 plot(Orange)
+
+# Normality Testing
+
+qqnorm(Orange$circumference)
+qqline(Orange$circumference)
+shapiro.test(Orange$circumference)
+
+qqnorm(Orange$age)
+qqline(Orange$age)
+shapiro.test(Orange$age)
+
+# histogram(Orange)
+# densityplot(Orange)
+# boxplot(Orange)
 
 my_gplot<- ggplot(Orange, aes(x=age, y=circumference))
 
-my_gplot + geom_point(aes(color=Tree)) +
-  # geom_smooth(method="lm", color="black", show.legend = T) +
+my_output<-my_gplot + geom_point(aes(color=Tree)) +
+
+# geom_smooth(method="lm", color="black", show.legend = T) +
 # loess < 1,000 item dataset < gam
 # geom_smooth(method="loess", color='green', show.legend = T) +
 # geom_smooth(method="glm", color='blue', show.legend =  T) 
 # glm generalized linear smooths
+  
   geom_smooth(method='lm', aes(color='linear')) +
   geom_smooth(method='lm', formula = y ~ poly(x,2), aes(color='polynomial')) +
   geom_smooth(method="lm", formula = y ~ x + I(x^2), aes(color='quadratic')) +
-  geom_smooth(method='lm', formula = y ~ x * log(x), aes(color='logarithmic')) +
+  geom_smooth(method='lm', formula = y ~ x * log(x), aes(color='logarithmic'))
+
 # Exponential not yet working  
-  geom_smooth(method='lm', formula = y ~ exp(x), aes(color='exponential')) +
+# geom_smooth(method='lm', formula = y ~ exp(x), aes(color='exponential'))
+
 # ggplots facets
-  facet_wrap(~Tree) +
+my_output + facet_grid(. ~ Tree)
+
+my_output + facet_grid(. ~ circumference)
+# facet_wrap(~Tree)
 # plot facets
-  Facet_wrap(~Tree)
+#  Facet_wrap(~Tree)
 # rlm, robust linear model, didn't see it work
 # Use geom_smooth instead of stat_geom for all standard plots.
