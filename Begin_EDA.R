953c046047f5d41124b3decd21e0c824423da8ee
Jeffrey Spinner <jeffglobalmedic@gmail.com>
2018-06-26 08:24:12 -0400

    Ordered Facet Plots

--- Begin_EDA.R
+++ Begin_EDA.R
@@ -2,6 +2,7 @@
 
 require(ggplot2)
 require(mgcv)
+require(dplyr)
 
 # Data set
 
@@ -26,6 +27,8 @@ shapiro.test(Orange$age)
 # densityplot(Orange)
 # boxplot(Orange)
 
+
+
 my_gplot<- ggplot(Orange, aes(x=age, y=circumference))
 
 my_output<-my_gplot + geom_point(aes(color=Tree)) +
@@ -45,11 +48,28 @@ my_output<-my_gplot + geom_point(aes(color=Tree)) +
 # geom_smooth(method='lm', formula = y ~ exp(x), aes(color='exponential'))
 
 # ggplots facets
-my_output + facet_grid(. ~ Tree)
+my_output + facet_wrap( ~ Tree, nrow = 2)
 
-my_output + facet_grid(. ~ circumference)
-# facet_wrap(~Tree)
+# my_output + facet_grid(. ~ circumference)
+
+# facet_wrap(~age, nrow = 2)
 # plot facets
 #  Facet_wrap(~Tree)
 # rlm, robust linear model, didn't see it work
 # Use geom_smooth instead of stat_geom for all standard plots.
+
+# Ordering Data
+
+ordered_Trees<-c(1,2,3,4,5)
+Orange2<- arrange(transform(Orange, Tree=factor(Tree,levels=ordered_Trees)),Tree)
+
+my_gplot<- ggplot(Orange2, aes(x=age, y=circumference))
+
+my_output<-my_gplot + geom_point(aes(color=Tree)) +
+  geom_smooth(method='lm', aes(color='linear')) +
+  geom_smooth(method='lm', formula = y ~ poly(x,2), aes(color='polynomial')) +
+  geom_smooth(method="lm", formula = y ~ x + I(x^2), aes(color='quadratic')) +
+  geom_smooth(method='lm', formula = y ~ x * log(x), aes(color='logarithmic'))
+
+# ggplots facets
+my_output + facet_wrap( ~ Tree, nrow = 2)
