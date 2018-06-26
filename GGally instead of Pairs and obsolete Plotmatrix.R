4d93f6433fa0a243b4f2720f7288f1668555aebf
Jeffrey Spinner <jeffglobalmedic@gmail.com>
2018-06-26 09:40:40 -0400

    Minor Fixes on Example Plots

--- GGally instead of Pairs and obsolete Plotmatrix.R
+++ GGally instead of Pairs and obsolete Plotmatrix.R
@@ -9,10 +9,11 @@ ggpairs(data=iris, # data.frame with variables
 
 ggpairs(data=tips, # data.frame with variables
         columns=1:3, # columns to plot, default to all.
-        title="tips data", # title of the plot
+        title="Tips Data", # title of the plot
         mapping = ggplot2::aes(color=sex)) # aesthetics, ggplot2 style?
 
 #--------------------------------------------------
+
 p_ <- function(pm) {
   if (interactive()) {
     print(pm)
@@ -25,11 +26,13 @@ pm <- ggpairs(tips[, 1:3])
 p_(pm)
 pm <- ggpairs(tips, 1:3, columnLabels = c("Total Bill", "Tip", "Sex"))
 p_(pm)
+# Every variable
 pm <- ggpairs(tips, upper = "blank")
 p_(pm)
 
 #--------------------------------------------------
 # Generic Matrix plot
+
 plotList <- list()
 for (i in 1:6) {
   plotList[[i]] <- ggally_text(paste("Plot #", i, sep = ""))
